84f919cdb746c3303a5cec04c15368c5402f51e5
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-14 12:23:50 +0530

    RecyclerAdapter Implemented

--- app/src/main/java/com/example/gohan/navcyclerview/MainActivity.java
+++ app/src/main/java/com/example/gohan/navcyclerview/MainActivity.java
@@ -1,17 +1,41 @@
 package com.example.gohan.navcyclerview;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    RecyclerView recyclerView;
+    DrawerLayout drawerLayout;
+    RecyclerView.Adapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    ArrayList<String> arrayList = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(toolbar);
+        // initialise recycler view
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // for improving performance of recycler view
+        recyclerView.setHasFixedSize(true);
+        String [] items =getResources().getStringArray(R.array.topics);
+        for(String Item:items)
+        {
+            arrayList.add(Item);
+        }
+        adapter=new RecyclerAdapter(arrayList);
+        recyclerView.setAdapter(adapter);
     }
 }
